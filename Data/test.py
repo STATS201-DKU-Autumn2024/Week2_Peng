b00af03b8ddbc8a66066cb866e700596eb29a2f8
王鹏 <wangpeng@Pengs-MacBook-Air.local>
2024-11-02 11:26:32 +0800

    test api

--- Data/test.py
+++ Data/test.py
@@ -1,1 +1,8 @@
-print('Hello World')
+# Use a pipeline as a high-level helper
+from transformers import pipeline
+
+messages = [
+    {"role": "user", "content": "Who are you?"},
+]
+pipe = pipeline("text-generation", model="gorilla-llm/gorilla-openfunctions-v2")
+pipe(messages)
